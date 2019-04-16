a9b5f47e6fb6d312c00cc2f5ef87adb2e73eee0e
Dennis Harvey <dennis.cameron.harvey@gmail.com>
2019-04-16 14:25:09 -0400

    ip lookup error messages

--- js/main.js
+++ js/main.js
@@ -100,12 +100,12 @@ function ipLookUp(callback) {
   $.ajax('http://ip-api.com/json')
   .then(
     function success(response) {
-      // console.log('User\'s Location Data is ', response);
+      console.log('User\'s Location Data is ', response);
       userData = response
       callback()
     },
     function fail(data, status) {
-      // console.log('Request failed.  Returned status of', status);
+      console.log('Request failed.  Returned status of', status);
       callback()
     }
   );
